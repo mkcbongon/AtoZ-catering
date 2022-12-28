fe3c7b189104e9d1397d7e19b880f0c02c4d11cc
Maria Bongon <kriszj58@gmail.com>
2022-12-28 12:35:59 +0800

    fix db

--- user/app/Models/Cart_model.php
+++ user/app/Models/Cart_model.php
@@ -8,9 +8,8 @@ class Cart_model extends Model
     protected $table = 'cart';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = 'true';
-    protected $allowedFields = ['package_id', 'img', 'item', 'status', 'client', 'contact', 'address', 'quantity', 'price'];
+    protected $allowedFields = ['package_id', 'img', 'item', 'product_stat', 'order_stat', 'client', 'contact', 'address', 'quantity', 'price'];
     protected $returnType = 'array';
-
 }
 
 ?>
