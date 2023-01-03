14f580d2363f72314d83ba8be36253760a31ef61
gioevora <giolo.evora@gmail.com>
2023-01-03 10:52:33 +0800

    admin

--- AtoZ/app/Views/include/admin/admin_end.php
+++ AtoZ/app/Views/include/admin/admin_end.php
@@ -7,29 +7,29 @@
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?=base_url('admin/plugins/bootstrap/js/bootstrap.bundle.min.js');?>"></script>
 <!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
+<script src="<?=base_url('admin/plugins/chart.js/Chart.min.js')?>"></script>
 <!-- Sparkline -->
-<script src="plugins/sparklines/sparkline.js"></script>
+<script src="<?=base_url('admin/plugins/sparklines/sparkline.js');?>"></script>
 <!-- JQVMap -->
-<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="<?=base_url('admin/plugins/jqvmap/jquery.vmap.min.js');?>"></script>
+<script src="<?=base_url('admin/plugins/jqvmap/maps/jquery.vmap.usa.js');?>"></script>
 <!-- jQuery Knob Chart -->
-<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="<?=base_url('admin/plugins/jquery-knob/jquery.knob.min.js');?>"></script>
 <!-- daterangepicker -->
-<script src="plugins/moment/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?=base_url('admin/plugins/moment/moment.min.js');?>"></script>
+<script src="<?=base_url('admin/plugins/daterangepicker/daterangepicker.js');?>"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?=base_url('admin/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js');?>"></script>
 <!-- Summernote -->
-<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<script src="<?=base_url('admin/plugins/summernote/summernote-bs4.min.js');?>"></script>
 <!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?=base_url('admin/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js');?>"></script>
 <!-- AdminLTE App -->
 <script src="<?=base_url('admin/js/adminlte.js');?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
+<script src="<?=base_url('admin/js/demo.js');?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="<?=base_url('admin/js/pages/dashboard.js');?>"></script>
 
