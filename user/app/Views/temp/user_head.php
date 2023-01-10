5b37903c5173315b0ee2dfd4652b80e5bfba24fc
Maria Bongon <kriszj58@gmail.com>
2023-01-10 13:57:21 +0800

    changing template

---
+++ user/app/Views/temp/user_head.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>A to Z Catering Services</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+    
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="users/img/logofinal.png">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="<?=base_url('users/lib/animate/animate.min.css');?>" rel="stylesheet">
+    <link href="<?=base_url('users/lib/owlcarousel/assets/owl.carousel.min.css');?>" rel="stylesheet">
+    <link href="<?=base_url('users/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css');?>" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="<?=base_url('users/css/bootstrap.min.css');?>" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="<?=base_url('users/css/style.css');?>" rel="stylesheet">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	
+
+</head>
