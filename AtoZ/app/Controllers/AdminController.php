d4c235f4104f1a4ed05930fcb993b2cc4c76968f
gioevora <giolo.evora@gmail.com>
2023-01-13 23:36:53 +0800

    Revert "h"
    This reverts commit 4b9804ac1c690d46e59f7a374e9ef32dd14eb36e, reversing
    changes made to 265bd3b98eb384264a157f4d7faea10d0d949207.

---
+++ AtoZ/app/Controllers/AdminController.php
@@ -0,0 +1,59 @@
+<?php 
+namespace App\Controllers;  
+use CodeIgniter\Controller;
+use App\Models\AdminModel;
+use App\Models\PackageModel;
+  
+class AdminController extends Controller
+{
+    public function index()
+    {
+        $session = session();
+        echo "Hello : ".$session->get('name');
+    }
+    public function dashboard()
+    {
+        $builder =  new AdminModel();
+        $data['booking'] = $builder->selectCount('id','totalbooking')->first();
+        return view('admin/dashboard', $data);
+        
+    }
+    public function display_bookings(){
+        return view('admin/bookings');
+    }
+    public function add_package()
+    {
+        return view('admin/package');
+    }
+    public function new_package()
+    {
+        $package_name = $this->request->getPost('package_name');
+        $package_details = $this->request->getPost('package_details');
+        $package_amount = $this->request->getPost('package_amount');
+        $package_image = $this->request->getFile('package_image');
+        $package_availability = $this->request->getPost('package_availability');
+
+        $package_image->move(FCPATH . 'uploads');
+        $pakage_data = [
+            'package_name' => $package_name,
+            'package_details' =>$package_details,
+            'package_amount' => $package_amount,
+            'package_image' => $package_image->getClientName(),
+            'package_availability' => $package_availability
+            
+        ];
+
+            $model = new PackageModel();
+            $model->save($pakage_data);
+
+            if(!$model){
+                return redirect()->back()->with('fail, something went wrong!');
+            }
+            else{
+                
+                return redirect()->to("add_package")->with('success', 'Successfully Added!');
+                
+            }
+
+    }
+}
