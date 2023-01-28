1298c72da49c59fb8eb5bdbcc8b4a2027970469d
Maria Bongon <kriszj58@gmail.com>
2023-01-28 19:35:15 +0800

    pahabol lang

--- AtoZ/app/Views/user/menu_old.php
+++ AtoZ/app/Views/user/menu_old.php
@@ -51,7 +51,7 @@
         <div class="product-cell price">Amount<button class="sort-button">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
           </button></div>
-        <div class="product-cell sales">Add to Cart<button class="sort-button">
+        <div class="product-cell sales">Select Food<button class="sort-button">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 512 512"><path fill="currentColor" d="M496.1 138.3L375.7 17.9c-7.9-7.9-20.6-7.9-28.5 0L226.9 138.3c-7.9 7.9-7.9 20.6 0 28.5 7.9 7.9 20.6 7.9 28.5 0l85.7-85.7v352.8c0 11.3 9.1 20.4 20.4 20.4 11.3 0 20.4-9.1 20.4-20.4V81.1l85.7 85.7c7.9 7.9 20.6 7.9 28.5 0 7.9-7.8 7.9-20.6 0-28.5zM287.1 347.2c-7.9-7.9-20.6-7.9-28.5 0l-85.7 85.7V80.1c0-11.3-9.1-20.4-20.4-20.4-11.3 0-20.4 9.1-20.4 20.4v352.8l-85.7-85.7c-7.9-7.9-20.6-7.9-28.5 0-7.9 7.9-7.9 20.6 0 28.5l120.4 120.4c7.9 7.9 20.6 7.9 28.5 0l120.4-120.4c7.8-7.9 7.8-20.7-.1-28.5z"/></svg>
           </button></div>
       </div>
@@ -75,10 +75,11 @@
         </div>
         <div class="product-cell price"><span class="cell-label">Price:</span>₱<?=$item['amount']?></div>
         <?php $session = session(); ?>
-          <?php if (session()->get('authGuard')): ?>
-            <div class="product-cell stock"><button class="btn btn-success addtocart" id="<?=$item['food_id']?>"><span class=""></span>Add to Cart</button></div>
+          <?php if (session()->get('isLoggedIn')): ?>
+            <div class="product-cell stock"><button class="btn btn-success select" id="<?=$item['food_id']?>"><span class=""></span>Select</button></div>
+            <!-- <?php if (".select"); ?> -->
           <?php else: ?>
-          <div class="product-cell stock"><a href="signin" class="btn btn-warning"><span class=""></span>Sign in to Order</a></div>
+          <div class="product-cell stock"><a href="signin" class="btn btn-warning"><span class=""></span>Sign in to Proceed</a></div>
           <?php endif ?>
       </div>
       <?php endforeach; ?>
